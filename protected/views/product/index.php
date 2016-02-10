39b0ed551632158dcef03e606f99a986c9acd659
p.manikanta swamy <swamy81293@gmail.com>
2016-02-10 19:16:43 +0530

    removed the links form product index

--- protected/views/product/index.php
+++ protected/views/product/index.php
@@ -5,11 +5,6 @@
 $this->breadcrumbs=array(
 	'Products',
 );
-
-$this->menu=array(
-	array('label'=>'Create Product', 'url'=>array('create')),
-	array('label'=>'Manage Product', 'url'=>array('admin')),
-);
 ?>
 
 <h1>Products</h1>
