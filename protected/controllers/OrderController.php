cd8d01ea8b6456269cb759f792ba4daed9a967a4
p.manikanta swamy <swamy81293@gmail.com>
2016-02-10 19:41:41 +0530

    written the code for give order a product  and if product is ordered email will send customer.

---
+++ protected/controllers/OrderController.php
@@ -0,0 +1,196 @@
+<?php
+
+class OrderController extends Controller
+{
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	public $layout='//layouts/column2';
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','view'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array('create','update'),
+				'users'=>array('@'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','delete'),
+				'users'=>array('admin'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+
+	/**
+	 * Displays a particular model.
+	 * @param integer $id the ID of the model to be displayed
+	 */
+	public function actionView($id)
+	{
+		$this->render('view',array(
+			'model'=>$this->loadModel($id),
+		));
+	}
+
+	/**
+	 * Creates a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionCreate()
+	{
+		
+
+		if(!empty($id)&& !Yii::app()->user->isGuest)
+		{
+			$customer_email=Yii::app()->user-name;
+			$customerModel=Customer::model()->findByAttributes(array('customer_email'=>$customer_email));
+			
+			$productId=$id;
+			$productModel=Product::model()->findByPk($productId);
+			
+			$model= new Order();
+			$model->order_product_id=$productModel->product_id;
+			$model->order_customer_id=$productModelr->customer_id;
+			$model->order_amount=$productModel->product_price+$productModel->product_shipping_price;
+			if ($model->save())
+			{
+				$attribuits=Order::model()->findByPk($model->order_id);
+			
+				$str="Product Name:{$productModel->product_name}\r\n".
+						"Order Id:{$attribuits->order_id}\r\n".
+						"Order Product Id:{$attribuits->order_product_id}\r\n".
+						"Order Customer Id:{$attribuits->order_custmer_id}\r\n".
+						"Total Amount With Shipping Charges:{$attribuits->order_amount}\r\n";
+				$message=new YiiMailMessage();
+				$message->subject="Your order details";
+				$message->setBody($str);
+				$message->addTo(Yii::app()->user->name);
+				$message->from=$costomerModel->customer_email;
+				Yii::app()->mail->send($message);
+				$this->redirect(array('view','id'=>$model->order_id));
+			
+			}
+			
+		}
+	
+		else {
+			echo "booking failed";
+		}
+	}
+
+	/**
+	 * Updates a particular model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id the ID of the model to be updated
+	 */
+	public function actionUpdate($id)
+	{
+		$model=$this->loadModel($id);
+
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model);
+
+		if(isset($_POST['Order']))
+		{
+			$model->attributes=$_POST['Order'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model->order_id));
+		}
+
+		$this->render('update',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Deletes a particular model.
+	 * If deletion is successful, the browser will be redirected to the 'admin' page.
+	 * @param integer $id the ID of the model to be deleted
+	 */
+	public function actionDelete($id)
+	{
+		$this->loadModel($id)->delete();
+
+		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+		if(!isset($_GET['ajax']))
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+	}
+
+	/**
+	 * Lists all models.
+	 */
+	public function actionIndex()
+	{
+		$dataProvider=new CActiveDataProvider('Order');
+		$this->render('index',array(
+			'dataProvider'=>$dataProvider,
+		));
+	}
+
+	/**
+	 * Manages all models.
+	 */
+	public function actionAdmin()
+	{
+		$model=new Order('search');
+		$model->unsetAttributes();  // clear any default values
+		if(isset($_GET['Order']))
+			$model->attributes=$_GET['Order'];
+
+		$this->render('admin',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer $id the ID of the model to be loaded
+	 * @return Order the loaded model
+	 * @throws CHttpException
+	 */
+	public function loadModel($id)
+	{
+		$model=Order::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
+
+	/**
+	 * Performs the AJAX validation.
+	 * @param Order $model the model to be validated
+	 */
+	protected function performAjaxValidation($model)
+	{
+		if(isset($_POST['ajax']) && $_POST['ajax']==='order-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+	}
+}
