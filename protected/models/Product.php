6a359f97358ef38eba90b5c75270f986edd8de0e
p.manikanta swamy <swamy81293@gmail.com>
2016-02-10 17:54:25 +0530

    generated model for product table

---
+++ protected/models/Product.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * This is the model class for table "product".
+ *
+ * The followings are the available columns in table 'product':
+ * @property integer $product_id
+ * @property string $product_name
+ * @property integer $product_price
+ * @property integer $product_shipping_price
+ *
+ * The followings are the available model relations:
+ * @property TblOrder[] $tblOrders
+ */
+class Product extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'product';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('product_name, product_price, product_shipping_price', 'required'),
+			array('product_price, product_shipping_price', 'numerical', 'integerOnly'=>true),
+			array('product_name', 'length', 'max'=>200),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('product_id, product_name, product_price, product_shipping_price', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'tblOrders' => array(self::HAS_MANY, 'TblOrder', 'order_product_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'product_id' => 'Product',
+			'product_name' => 'Product Name',
+			'product_price' => 'Product Price',
+			'product_shipping_price' => 'Product Shipping Price',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('product_id',$this->product_id);
+		$criteria->compare('product_name',$this->product_name,true);
+		$criteria->compare('product_price',$this->product_price);
+		$criteria->compare('product_shipping_price',$this->product_shipping_price);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Product the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
